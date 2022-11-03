bbe93ec1b419058f1cf5453028a955cb8a61c1e5
Germano B <germanobenini2@hotmail.com>
2022-11-03 19:50:02 -0300

    Prepara deploy

--- src/pages/solicitacao/index.tsx
+++ src/pages/solicitacao/index.tsx
@@ -7,11 +7,22 @@ import { Button } from 'react-bootstrap'
 import { Container, Row, Col } from 'react-grid-system'
 import { AuthContext } from '../../contexts/AuthContext'
 import { api, apiAuth } from '../../services/api'
+import Form from 'react-bootstrap/Form'
+import { useForm } from 'react-hook-form'
 
-const Form: React.FC = () => {
+interface IFormData {
+  formBasicEmail: string
+}
+
+const FormComponent: React.FC = () => {
   const { isAuthenticated, user } = useContext(AuthContext)
 
   const router = useRouter()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm()
 
   // TODO: Descomentar ao final do desenvolvimento
   // useEffect(() => {
@@ -19,6 +30,10 @@ const Form: React.FC = () => {
   //     router.push('/login')
   //   }
   // }, [isAuthenticated, router])
+
+  const onSubmit = async (data: IFormData) => {
+    console.log(data)
+  }
 
   return (
     <>
@@ -45,11 +60,133 @@ const Form: React.FC = () => {
             </p>
           </Col>
         </Row>
-        <Row align="center">
+        <Row style={{ marginRight: 0 }}>
           <Col xs={12}>
-            <Row style={{ justifyContent: 'center' }}>
-              Aqui vai listar todas dash
-            </Row>
+            <Form onSubmit={handleSubmit(onSubmit)}>
+              <Col xs={10}>
+                <Form.Group controlId="formBasicEmail">
+                  <Form.Label>Informe o fornecedor</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="Pesquisar por CNPJ/CPF sem pontuação"
+                    {...register('supplier')}
+                  />
+                </Form.Group>
+              </Col>
+              <Col xs={10}>
+                <Form.Group controlId="formBasicEmail">
+                  <Form.Label>Classificação</Form.Label>
+                  <Form.Select
+                    {...register('ranking')}
+                    style={{
+                      display: 'block',
+                      width: '100%',
+                      padding: '.375rem 2.25rem .375rem .75rem',
+                      // -moz-padding-start: calc('.75rem - 3px')
+                      fontSize: '1rem',
+                      fontWeight: '400',
+                      lineHeight: '1.5',
+                      color: '#212529',
+                      backgroundColor: '#fff',
+                      // backgroundImage: url(
+                      //   "data:image/svg+xml;charset=utf-8,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16'%3E%3Cpath fill='none' stroke='%23343a40' stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='m2 5 6 6 6-6'/%3E%3C/svg%3E"
+                      // ),
+                      backgroundRepeat: 'no-repeat',
+                      backgroundPosition: 'right .75rem center',
+                      backgroundSize: '16px 12px',
+                      border: ' 1px solid #ced4da',
+                      borderRadius: '.375rem',
+                      transition:
+                        'border-color .15s ease-in-out,box-shadow .15s ease-in-out',
+                      // -webkit-appearance: 'none'
+                      appearance: 'none'
+                    }}
+                  >
+                    <option>Selecione uma opção...</option>
+                    <option value="1">Bens e consumo</option>
+                    <option value="2">Serviços</option>
+                  </Form.Select>
+                </Form.Group>
+              </Col>
+              <Col xs={10}>
+                <Form.Group controlId="formBasicEmail">
+                  <Form.Label>Assunto</Form.Label>
+                  <Form.Select
+                    {...register('subject')}
+                    style={{
+                      display: 'block',
+                      width: '100%',
+                      padding: '.375rem 2.25rem .375rem .75rem',
+                      // -moz-padding-start: calc('.75rem - 3px')
+                      fontSize: '1rem',
+                      fontWeight: '400',
+                      lineHeight: '1.5',
+                      color: '#212529',
+                      backgroundColor: '#fff',
+                      // backgroundImage: url(
+                      //   "data:image/svg+xml;charset=utf-8,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16'%3E%3Cpath fill='none' stroke='%23343a40' stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='m2 5 6 6 6-6'/%3E%3C/svg%3E"
+                      // ),
+                      backgroundRepeat: 'no-repeat',
+                      backgroundPosition: 'right .75rem center',
+                      backgroundSize: '16px 12px',
+                      border: ' 1px solid #ced4da',
+                      borderRadius: '.375rem',
+                      transition:
+                        'border-color .15s ease-in-out,box-shadow .15s ease-in-out',
+                      // -webkit-appearance: 'none'
+                      appearance: 'none'
+                    }}
+                  >
+                    <option>Selecione uma opção...</option>
+                    <option value="1">
+                      Renegociação de produtos/ serviços
+                    </option>
+                    <option value="2">Renegociação da dívida</option>
+                  </Form.Select>
+                </Form.Group>
+              </Col>
+              <Col xs={10}>
+                <Form.Group controlId="formBasicEmail">
+                  <Form.Label>Tipo de problema</Form.Label>
+                  <Form.Select
+                    {...register('kindOfProblem')}
+                    style={{
+                      display: 'block',
+                      width: '100%',
+                      padding: '.375rem 2.25rem .375rem .75rem',
+                      // -moz-padding-start: calc('.75rem - 3px')
+                      fontSize: '1rem',
+                      fontWeight: '400',
+                      lineHeight: '1.5',
+                      color: '#212529',
+                      backgroundColor: '#fff',
+                      // backgroundImage: url(
+                      //   "data:image/svg+xml;charset=utf-8,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16'%3E%3Cpath fill='none' stroke='%23343a40' stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='m2 5 6 6 6-6'/%3E%3C/svg%3E"
+                      // ),
+                      backgroundRepeat: 'no-repeat',
+                      backgroundPosition: 'right .75rem center',
+                      backgroundSize: '16px 12px',
+                      border: ' 1px solid #ced4da',
+                      borderRadius: '.375rem',
+                      transition:
+                        'border-color .15s ease-in-out,box-shadow .15s ease-in-out',
+                      // -webkit-appearance: 'none'
+                      appearance: 'none'
+                    }}
+                  >
+                    <option>Selecione uma opção...</option>
+                    <option value="1">Dívida ativa</option>
+                    <option value="2">Inadimplência</option>
+                  </Form.Select>
+                </Form.Group>
+              </Col>
+
+              <Col>
+                <Button variant="primary" type="submit">
+                  Submit
+                </Button>
+              </Col>
+            </Form>
           </Col>
         </Row>
       </Container>
@@ -57,7 +194,7 @@ const Form: React.FC = () => {
   )
 }
 
-export default Form
+export default FormComponent
 
 export const getServersideProps: GetServerSideProps = async ctx => {
   const { ['redisputing.token']: token } = parseCookies(ctx)
